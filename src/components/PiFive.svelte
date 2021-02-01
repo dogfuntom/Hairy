0d506c0fdb2beed5212f94462b70f9a7f45db3a5
Maxim Kamalov <MaximKamalov@outlook.com>
2021-02-02 01:20:42 +0300

    🎉 integrated p5 into sapper,
    heavily inspired by p5-svelte

---
+++ src/components/PiFive.svelte
@@ -0,0 +1,19 @@
+<script>
+  import _p5 from './PiFiveAction';
+  export let sketch = null;
+  export let engine = null;
+
+  function foo(node, workspace) {
+    engine = _p5(node, workspace);
+    return engine;
+  }
+</script>
+
+<figure use:foo={sketch} />
+
+<style>
+  figure {
+    display: inline;
+    margin: 0;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,49 +1,100 @@
 <script>
-	import successkid from "images/successkid.jpg";
 	import { onMount } from "svelte";
 
 	let P5;
 
 	onMount(async () => {
-		const module = await import("p5-svelte");
+		const module = await import("../components/PiFive.svelte");
 		P5 = module.default;
 	});
 
-	let width = 55;
+	let noiseScale = 0.0625;
 	let height = 55;
 
-	const sketch = (p5) => {
-		p5.setup = () => {
-			p5.createCanvas(400, 400);
+	const sketch = (it) => {
+		const width = 400;
+		const height = 400;
+		const pointsNumber = 128;
+		const points = Array(pointsNumber);
+		let g = null;
+		const randomPoint = () => {
+			const shouldSkip = (tupledArg) => {
+				const pixels = tupledArg[0];
+				const x = tupledArg[1] | 0;
+				const y = tupledArg[2] | 0;
+				const i = ((y * height + x) * 4) | 0;
+				return ~~pixels[i + 3] < 127;
+			};
+			let x_1 = 0;
+			let y_1 = 0;
+			while (
+				((x_1 = ~~Math.floor(it.random(width)) | 0),
+				((y_1 = ~~Math.floor(it.random(height)) | 0),
+				shouldSkip([g.pixels, x_1, y_1])))
+			) {}
+			return it.createVector(x_1, y_1);
 		};
-
-		p5.draw = () => {
-			p5.ellipse(p5.width / 2, p5.height / 2, width, height);
+		it.setup = () => {
+			it.createCanvas(width, height, "p2d");
+			it.background(200);
+			it.noiseSeed(10);
+			it.noStroke();
+			it.fill(0);
+			g = it.createGraphics(width, height, "p2d");
+			g.textSize(300);
+			g.text("G", it.width / 32, it.height / 1.3);
+			g.loadPixels();
+			for (let i_1 = 0; i_1 <= pointsNumber - 1; i_1++) {
+				points[i_1] = randomPoint();
+			}
+		};
+		it.draw = () => {
+			it.image(g, 0, 0);
+			points.forEach((p) => {
+				it.circle(p.x, p.y, 0.5);
+			});
+			for (let i_2 = 0; i_2 <= pointsNumber - 1; i_2++) {
+				let p_1 = points[i_2];
+				p_1 = p_1.add(
+					it.createVector(
+						2 *
+							(it.noise(
+								p_1.x * noiseScale,
+								p_1.y * noiseScale,
+								0
+							) *
+								2 -
+								1),
+						2 *
+							(it.noise(
+								p_1.x * noiseScale,
+								p_1.y * noiseScale,
+								9
+							) *
+								2 -
+								1)
+					)
+				);
+				points[i_2] = p_1;
+				if (it.random() < 0.0625) {
+					points[i_2] = randomPoint();
+				}
+			}
 		};
 	};
+
+	let engine;
 </script>
 
 <svelte:head>
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt="Success Kid" src={successkid} />
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p>
-	<strong
-		>Try editing this file (src/routes/index.svelte) to test live reloading.</strong
-	>
-</p>
-
 <label>
-	Width
-	<input type="range" bind:value={width} min="100" max="1000" step="0.01" />
-	{width}
+	Noise scale
+	<input type="range" bind:value={noiseScale} min="{0.0625/4}" max="{0.0625*4}" step="0.01"
+		on:change="{engine.frameRate(engine.frameRate()-1)}" />
+	{noiseScale}
 </label>
 
 <label>
@@ -52,8 +103,7 @@
 	{height}
 </label>
 
-<!--<P5 {sketch} />-->
-<svelte:component this={P5} {sketch} />
+<svelte:component this={P5} {sketch} bind:engine={engine} />
 
 <style>
 	h1,
